9c74ebc5834e87800e09dc28c00511125bdb9d6f
Pedro Burón <pedro@witoi.com>
2012-12-02 04:55:54 -0300

    fixed urls for admin

--- pythontoolbox/urls.py
+++ pythontoolbox/urls.py
@@ -7,12 +7,13 @@ from django.views.generic import TemplateView
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+
     url(r'', include('tools.urls', namespace='tools', app_name='tools')),
 
     url(r'', include('social_auth.urls')),
 
     url(r'^accounts/', include('accounts.urls', namespace='accounts', app_name='accounts')),
 
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^admin/', include(admin.site.urls)),
 )
